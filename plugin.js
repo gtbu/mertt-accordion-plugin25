44697df7a8763113dbdae2c1a4d172c6b5f10bc4
Merter GÜLBAHAR <merter@gazi.edu.tr>
2024-12-21 15:14:42 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -39,9 +39,3 @@ CKEDITOR.plugins.add('merttaccordion', {
       CKEDITOR.dialog.add( 'maccDialog', this.path + 'dialogs/merttaccordion.js' );
 }});
 
-  function generateUUID() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-  }
-
